916c19725533048dd2ef74eb77b4e153e83f413e
Swaroopsbandekar <103209524+Swaroopsbandekar@users.noreply.github.com>
2023-06-06 21:05:58 +0530

    styling

--- src/componets/Navbar.js
+++ src/componets/Navbar.js
@@ -2,10 +2,11 @@ import React from 'react'
 import img1 from '../images/img-1.jpg';
 
 
+
 function Navbar() {
   return (
    <>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+    <nav id='menu' className="navbar navbar-expand-lg navbar-dark bg-dark ">
   <div className="container-fluid " style={{fontSize:'20px'}}>
       <img className='img1' src={img1} alt="img" width="50" height="40"/>
     <a className="navbar-brand"  style={{fontSize:'25px'}} href="/">Tribute Page</a>
@@ -15,14 +16,14 @@ function Navbar() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a rel="noreferrer" className="nav-link " aria-current="page"  href="#About">About</a>
+          <a rel="noreferrer" className="nav-link " aria-current="page"  href="#About">  <span> About</span>  </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#Personal-info">Personal Info</a>
+          <a className="nav-link" href="#Personal-info"> <span> Personal Info</span></a>
         </li>
 
         <li className="nav-item">
-          <a className="nav-link" href="#Timeline">Timeline</a>
+          <a className="nav-link" href="#Timeline"> <span> Timeline</span></a>
         </li>
 
       
